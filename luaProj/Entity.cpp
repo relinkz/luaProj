0b15f9ab14130ca045bda6458e2a79ec4ab18eab
relinkz <seb.lundgren@gmail.com>
2016-04-13 12:27:48 +0200

    new classes

---
+++ luaProj/Entity.cpp
@@ -0,0 +1,14 @@
+#include "Entity.h"
+
+
+
+Entity::Entity()
+{
+	this->xPos = 0;
+	this->yPos = 0;
+}
+
+
+Entity::~Entity()
+{
+}
---
+++ luaProj/Entity.h
@@ -0,0 +1,14 @@
+#ifndef ENTITY_H
+#define ENTITY_H
+class Entity
+{
+private:
+	float xPos;
+	float yPos;
+public:
+	Entity();
+	virtual ~Entity();
+};
+
+#endif
+
